75ec2ad57c7c029a5ba37cbaef73b8b6bf87164c
vinay564 <38555014+vinay564@users.noreply.github.com>
2021-10-27 20:29:05 -0500

    made changes in user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var a =0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  a++;
+  res.send('User accesses are:'+a);
+
 });
+
+
 
 module.exports = router;
